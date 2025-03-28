a0dabc38485efceb0009eda4e217001550e6a91c
Sebastian Björkelid <sebastian.bjorkelid@gmail.com>
2025-03-28 14:49:26 +0100

    Added functionality for user input and scoring system in the math game. Enhanced UI elements for better user experience and fixed minor bugs.

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [{
+    name: "matte-spel",
+    script: "serve",
+    env: {
+      PM2_SERVE_PATH: '.',
+      PM2_SERVE_PORT: 3000,
+      PM2_SERVE_SPA: 'true',
+      PM2_SERVE_HOMEPAGE: '/index.html'
+    }
+  }]
+} 
