ea471e04e09e3679e65173ff2c4ede4f259d085f
Sebastian Björkelid <sebastian.bjorkelid@gmail.com>
2025-03-28 14:51:09 +0100

    Update PM2 serve port from 3000 to 3333 in ecosystem configuration

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     name: "matte-spel",
     script: "serve",
     env: {
-      PM2_SERVE_PATH: '.',
+      PM2_SERVE_PATH: './dist',
       PM2_SERVE_PORT: 3333,
       PM2_SERVE_SPA: 'true',
       PM2_SERVE_HOMEPAGE: '/index.html'
